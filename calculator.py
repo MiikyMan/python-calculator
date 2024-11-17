2a50c652eb6a55f10129f233b956dc7136c74ecf
Phuris Pimpat <89836890+MiikyMan@users.noreply.github.com>
2024-11-17 14:06:35 +0000

    test cases added & bug fixed.

--- calculator.py
+++ calculator.py
@@ -3,32 +3,89 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        return a - b
 
     def multiply(self, a, b):
         result = 0
-        for i in range(b+1):
+
+        if a == 0 or b == 0:  
+            return 0
+    
+        # ใช้ XOR หากพบว่าตัวใดตัวหนึ่งติดลบจะต้องได้คำตอบที่ติดลบ
+        ShouldItBeNegative = (a < 0) ^ (b < 0) 
+
+        a = -a if a < 0 else a
+        b = -b if b < 0 else b
+
+        for i in range(b):
             result = self.add(result, a)
-        return result
+
+        return -result if ShouldItBeNegative else result
 
     def divide(self, a, b):
         result = 0
-        while a > b:
+
+        if b == 0:  
+            return None
+
+        # ใช้ XOR หากพบว่าตัวใดตัวหนึ่งติดลบจะต้องได้คำตอบที่ติดลบ
+        ShouldItBeNegative = (a < 0) ^ (b < 0)
+
+        a = -a if a < 0 else a
+        b = -b if b < 0 else b
+
+        while a >= b:
             a = self.subtract(a, b)
             result += 1
-        return result
-    
+
+        return -result if ShouldItBeNegative else result
+
     def modulo(self, a, b):
-        while a <= b:
-            a = a-b
-        return a
+        """
+            แบ่งเป็น 4 กรณี
+                1. - mod - 
+                2. + mod +
+                3. + mod - 
+                4. - mod + 
+            จากที่ทำการทดลองพบว่ากรณี (- mod +) = -(+ mod -)
+        """
+        og_b = b
+
+        if (a<0 and b<0) :   # กรณีแรก: ทั้งคู่มีค่าติดลบ
+            result = a - self.multiply(self.divide(a,b),b)
+            return result
+        
+        # กรณี (+ mod -)
+        if b < 0 and a > 0:  # กลับให้เป็นกรณี (- mod +) 
+            a = -a
+            b = -b
+        
+        # ที่เหลือทั้งสามกรณีใช้วิธีเดียวกัน
+        if b == 0: # ดักกรณี b เป็น 0
+            return None
+        
+        b = -b if b < 0 else b
+
+        # คำนวณผลเศษ
+        dividedResult = self.divide(a, b)
+        remainder = a - self.multiply(dividedResult, b)
+        
+        # หากเศษติดลบให้บวกด้วยค่า b
+        if remainder < 0:
+            remainder += b
+        
+        # กรณี 3. (+ mod -) จะได้คำตอบติดลบเสมอ
+        if og_b < 0:
+            return -remainder
+        
+        return remainder
 
 # Example usage:
 if __name__ == "__main__":
     calc = Calculator()
     print("This is a simple calculator class!")
-    print("Example: addition: ", calc.add(1, 2))
-    print("Example: subtraction: ", calc.subtract(4, 2))
-    print("Example: multiplication: ", calc.multiply(2, 3))
-    print("Example: division: ", calc.divide(10, 2))
-    print("Example: modulo: ", calc.modulo(10, 3))
+    print("Example: addition: ", calc.add(1, 2))          # Output: 3
+    print("Example: subtraction: ", calc.subtract(4, 2))  # Output: 2
+    print("Example: multiplication: ", calc.multiply(-2, 3))  # Output: -6
+    print("Example: division: ", calc.divide(-10, 2))     # Output: -5
+    print("Example: modulo: ", calc.modulo(-10, 3))       # Output: -1
--- test_calculator.py
+++ test_calculator.py
@@ -7,7 +7,48 @@ class TestCalculator(unittest.TestCase):
         self.calc = Calculator()
 
     def test_add(self):
-        self.assertEqual(self.calc.add(1, 2), 3)
+        self.assertEqual(self.calc.add(1, 2), 1+2)
+        self.assertEqual(self.calc.add(3, -4), 3+(-4))
+        self.assertEqual(self.calc.add(3, 0), 3+0)
+
+    def test_substract(self):
+        self.assertEqual(self.calc.subtract(2, 0), 2-0)
+        self.assertEqual(self.calc.subtract(3, 10), 3-10)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(1, -3), 1*-3)    # b เป็นลบ
+        self.assertEqual(self.calc.multiply(-3, -2), -3*-2)    # a และ b เป็นลบ
+        self.assertEqual(self.calc.multiply(-1, 3), -1*3)    # a เป็นลบ
+        self.assertEqual(self.calc.multiply(3, 5), 3*5)     # แบบปกติ
+        self.assertEqual(self.calc.multiply(3, 0), 3*0)      # a คูณด้วย 0
+        self.assertEqual(self.calc.multiply(0, 3), 0*3)      # b คูณด้วย 0
+        self.assertEqual(self.calc.multiply(0, 0), 0*0)      # a และ b เป็น 0
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(2, 1), 2)        # หารลงตัว
+        self.assertEqual(self.calc.divide(30, 7), 4)       # หารไม่ลงตัว
+        self.assertEqual(self.calc.divide(0, 2), 0)        # a เป็น 0
+        self.assertEqual(self.calc.divide(2, 0), None)     # a หารด้วย 0
+        self.assertEqual(self.calc.divide(0, 0), None)     # 0 หารด้วย 0
+        self.assertEqual(self.calc.divide(7, 7), 1)        # ตัวเศษและส่วนเท่ากัน 
+        self.assertEqual(self.calc.divide(-3, -3), 1)      # a และ b เป็นลบ
+        self.assertEqual(self.calc.divide(-3, 4), 0)       # a เป็นลบ
+        self.assertEqual(self.calc.divide(3, -4), 0)      # b เป็นลบ
+        self.assertEqual(self.calc.divide(-4, 3), -1)       # a เป็นลบ
+        self.assertEqual(self.calc.divide(4, -3), -1)      # b เป็นลบ
+
+    def test_modulo(self):
+        self.assertEqual(self.calc.modulo(7, 0), None)     # mod ด้วย 0
+        self.assertEqual(self.calc.modulo(4, 10), 4%10)    # mod ด้วยจำนวนที่มากกว่า
+        self.assertEqual(self.calc.modulo(4, 3), 4%3)      # mod แบบธรรมดา
+        self.assertEqual(self.calc.modulo(4, 4), 4%4)      # a mod a
+        self.assertEqual(self.calc.modulo(0, 0), None)     # 0 mod 0
+        self.assertEqual(self.calc.modulo(-4, 3), -4%3)    # - mod +
+        self.assertEqual(self.calc.modulo(4, -3), 4%-3)    # + mod -
+        self.assertEqual(self.calc.modulo(-5, -3), -5%-3)  # - mod -
+        self.assertEqual(self.calc.modulo(5, 3), 5%3)      # + mod +
+        self.assertEqual(self.calc.modulo(-5, -6), -5%-6)  # - mod -
+        self.assertEqual(self.calc.modulo(5, 6), 5%6)      # + mod +
 
     # Add the following test methods to the TestCalculator class:
 
